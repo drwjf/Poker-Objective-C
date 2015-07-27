f4d609f59f188611655ed41d0faa68f3ec02703c
schurik77799 <admin@Mac-Admin.local>
2015-07-27 21:06:05 +0300

    In PlayViewController was added code, that modify corner radius of view's.

--- Poker/PlayGameViewController.m
+++ Poker/PlayGameViewController.m
@@ -21,7 +21,7 @@
 
 @property (strong, nonatomic) IBOutletCollection(UIImageView) NSArray *arrayOfImagesPrivatePlayersCard;
 
-
+@property (nonatomic) int countOfPlayersOnTheTable;
 //@property (strong, nonatomic) IBOutletCollection(UIImageView) NSArray *arrayOfPlayersImages;
 //@property (strong, nonatomic) IBOutletCollection(UILabel) NSArray *arrayOfLabelsPlayersNames;
 //@property (strong, nonatomic) IBOutletCollection(UILabel) NSArray *arrayOfLabelsPlayersMoney;
@@ -31,14 +31,81 @@
 
 //@property (weak, nonatomic) IBOutlet UILabel *labelOfMoneyOnTheTAble;
 
+
 @end
 
 @implementation PlayGameViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    [self changeCornerRadiusOfPlayersViews:10];
+    [self rotateRightPrivateCardOfPlayers:10];
+    [self changeCornerRadiusOfCards:10];
 }
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+}
+
+
+- (void)clearTable
+{
+    //performance better, than if we use a (1)cycle.
+    for(UILabel *nameLabel in self.arrayOfLabelsPlayersNames)   [nameLabel  setAlpha:0.0];
+    for(UILabel *moneyLabel in self.arrayOfLabelsPlayersMoneys) [moneyLabel setAlpha:0.0];
+    for(UIImageView *imageView in self.arrayOfPlayersImages)    [imageView  setAlpha:0.0];
+    for(UIImageView *privateCardImage in self.arrayOfImagesPrivatePlayersCard) [privateCardImage setAlpha:0.0];
+}
+
+#define ANGLE_PI_devided_4 0.19625
+
+- (void)rotateRightPrivateCardOfPlayers: (int)countOfPlayers
+{
+    UIImageView *card;
+    
+    for(int i=0; i<countOfPlayers; i++) {
+        card = [_arrayOfImagesPrivatePlayersCard objectAtIndex:i*2];
+        card.transform = CGAffineTransformRotate(card.transform, ANGLE_PI_devided_4);
+    }
+}
+
+- (void)setCornerRadius:(UIView *)view andRadius:(int)radius
+{
+    [view.layer setMasksToBounds:YES];
+    [view.layer setCornerRadius:radius];
+}
+
+#define CORNER_RADIUS_OF_PLAYER_ICON 10
+
+- (void)changeCornerRadiusOfPlayersViews:(int)count
+{
+    UIView *playersNameLabel, *playersMoneyLabel, *playersImage;
+    
+    for(int i=0; i<count; i++) {
+        playersImage       = [self.arrayOfPlayersImages       objectAtIndex:i];
+        playersNameLabel   = [self.arrayOfLabelsPlayersNames  objectAtIndex:i];
+        playersMoneyLabel  = [self.arrayOfLabelsPlayersMoneys objectAtIndex:i];
+        
+        [self setCornerRadius:playersImage       andRadius:CORNER_RADIUS_OF_PLAYER_ICON];
+        [self setCornerRadius:playersNameLabel   andRadius:CORNER_RADIUS_OF_PLAYER_ICON];
+        [self setCornerRadius:playersMoneyLabel  andRadius:CORNER_RADIUS_OF_PLAYER_ICON];
+    }
+}
+
+#define CORNER_RADIUS_CARD 3
+
+- (void)changeCornerRadiusOfCards: (int)countOfPlayers
+{
+    for(UIImageView *cardOnTheTable in self.arrayOfImagesCardsOnTheTable)
+        [self setCornerRadius:cardOnTheTable andRadius:CORNER_RADIUS_CARD];
+    
+    for (int i=0; i<countOfPlayers; i++)
+        [self setCornerRadius:[self.arrayOfImagesPrivatePlayersCard objectAtIndex:i*2] andRadius:CORNER_RADIUS_CARD];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
